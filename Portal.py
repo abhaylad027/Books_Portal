e700e2d4eaaf4561b29ea1400f49e9be1ee2c784
abhay <abhaylad022@gmail.com>
2021-04-05 00:59:58 +0530

    1st endpoint almost completed

--- Portal.py
+++ Portal.py
@@ -1,20 +1,45 @@
 from flask import Flask, redirect
+from random import random, sample
 import pandas as pd
+import json
+
+import local_setting as config
+
+csv_file = config.csv_file["file_path"]
 
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello World!!"
 
-@app.route("/all_books")
-def read_csv():
-    df = pd.read_csv(r"D:\abhay\Interview\ICAV\Books_Portal\data\books.csv")
-    print(df.to_string())
-    return df.to_string()
+
+@app.route("/get_books/<numbers>")
+def read_csv(numbers):
+    # print(numbers,type(numbers))
+    # df = pd.read_csv(r"D:\abhay\Interview\ICAV\Books_Portal\data\books.csv",nrows=int(numbers))
+    df = pd.read_csv(csv_file, nrows=int(numbers))
+    # df_book = pd.DataFrame[{'books':[1]}]
+
+    # Cleaning data as empty into null in csv
+    # cleaned_df = df.fillna("null")
+    # result = cleaned_df.to_json(orient="records")
+
+    result = df.to_json(orient="records")
+    # df.to_json('books.json',orient="records",)
+    final_result = {"books": json.loads(result)}
+
+    # dj = json.loads(result)
+    # print("dj",dj)
+    # dc = {"books":dj}
+    # print(dc)
+
+
+    return final_result
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True,port=6000)
+    app.run(debug=True)
---
+++ local_setting.py
@@ -0,0 +1,5 @@
+# From this local setting file we can configure parameters which are application independent
+# Or which we need to change without disturbing main code
+
+# path of csv file
+csv_file = {"file_path": r"D:\abhay\Interview\ICAV\Books_Portal\data\books.csv"}
