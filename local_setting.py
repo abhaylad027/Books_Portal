eeb4e6b658cebffa3113a5b24866e1beeea80b58
abhay <abhaylad022@gmail.com>
2021-04-07 10:47:36 +0530

    added some comments in local_setting

--- local_setting.py
+++ local_setting.py
@@ -1,8 +1,12 @@
 # From this local setting file we can configure parameters which are application independent
 # Or which we need to change without disturbing main code
 
-# path of csv file
+# path of csv file in r" "
+# please change this path at the time of running code as location of csv file if required
 csv_file = {"file_path": r"D:\abhay\Interview\ICAV\Books_Portal\data\books.csv"}
 
 # configurable port number to run
+# please change port number as per available port number if required
 custom_port = 5067
+
+
